c1987f33143968ec38369a38de7db10d5947ab4b
Xav-la-program <xavier.meyer@outlook.fr>
2021-11-29 12:36:20 +0100

    payoff header creation

--- payoff.h
+++ payoff.h
@@ -1,6 +1,39 @@
 #ifndef PAYOFF_H_INCLUDED
 #define PAYOFF_H_INCLUDED
+#include <vector>
 
 
+namespace payoff
+{
+    class Payoff
+    {
+    public :
+        Payoff() default;
+        virtual std::vector<double> compute_payoff (std::vector<double>& spot) const = 0;
+        ~Payoff() default;
 
+    };
+
+    class Call: public Payoff
+    {
+    public:
+        Call(double K);
+        ~Call() default;
+    private :
+        double m_strike;
+
+
+    };
+
+    class Put: public Payoff
+    {
+    public:
+        Put(double K);
+        ~Put() default;
+    private :
+        double m_strike;
+
+
+    };
+}
 #endif // PAYOFF_H_INCLUDED
