a4e5c07d220ead3dd2b6a36c50bb1522d1e86b52
AymericBerthoumieu <aym3ric2009@hotmail.fr>
2021-12-20 14:26:05 +0100

    dirichlet boundary conditions test

--- BoundaryCondition.cpp
+++ BoundaryCondition.cpp
@@ -4,22 +4,22 @@
 
 namespace boundary
 {
-    std::vector<std::vector<double>> BoundaryCondition::get_conditions(int time, double space)
+    std::vector<std::vector<double>> BoundaryCondition::get_conditions(int time, double space, int length) const
     {
-        std::vector<double> coef_Xt = BoundaryCondition::get_coef_Xt(time, space);
-        std::vector<double> coef_Xt1 = BoundaryCondition::get_coef_Xt1(time, space);
+        std::vector<double> coef_Xt = BoundaryCondition::get_coef_Xt(time, space, length);
+        std::vector<double> coef_Xt1 = BoundaryCondition::get_coef_Xt1(time, space, length);
         std::vector<std::vector<double>> conditions = {coef_Xt, coef_Xt1};
         return conditions;
     }
 
-    std::vector<double> BoundaryCondition::get_coef_Xt(int time, double space)
+    std::vector<double> BoundaryCondition::get_coef_Xt(int time, double space, int length) const
     {
         //todo
         std::vector<double> condition;
         return condition;
     }
 
-    std::vector<double> BoundaryCondition::get_coef_Xt1(int time, double space)
+    std::vector<double> BoundaryCondition::get_coef_Xt1(int time, double space, int length) const
     {
         //todo
         std::vector<double> condition;
@@ -27,4 +27,20 @@ namespace boundary
     }
 
     //TODO: implementer un throw dans le cas où les variables ne sont pas déclarées
+
+    std::vector<double> Dirichlet_test::get_coef_Xt(int time, double space, int length) const
+    {
+        // f / [coefs]
+        std::vector<double> res(length + 1, 0.0);
+        res[0] = space;
+        return res;
+    }
+
+    std::vector<double> Dirichlet_test::get_coef_Xt1(int time, double space, int length) const
+    {
+        // f / [coefs]
+        std::vector<double> res(length + 1, 0.0);
+        res[0] = space;
+        return res;
+    }
 }
--- BoundaryCondition.h
+++ BoundaryCondition.h
@@ -11,12 +11,19 @@ namespace boundary
     public:
         BoundaryCondition() = default;
         ~BoundaryCondition() = default;
-        std::vector<std::vector<double>> get_conditions(int time, double space); //calcul des conditions aux bornes à chaque itération
+        std::vector<std::vector<double>> get_conditions(int time, double space, int length) const; //calcul des conditions aux bornes à chaque itération
         //Potential TODO: calculer les conditions aux bornes d'un coup
 
     protected:
-        virtual std::vector<double> get_coef_Xt(int time, double space);
-        virtual std::vector<double> get_coef_Xt1(int time, double space);
+        virtual std::vector<double> get_coef_Xt(int time, double space, int length) const;
+        virtual std::vector<double> get_coef_Xt1(int time, double space, int length) const;
+    };
+
+    class Dirichlet_test: public BoundaryCondition
+    {
+    protected:
+        virtual std::vector<double> get_coef_Xt(int time, double space, int length) const override;
+        virtual std::vector<double> get_coef_Xt1(int time, double space, int length) const override;
     };
 }
 
